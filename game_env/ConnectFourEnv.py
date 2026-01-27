da456f3f1ed0fe7c2636d15050b8638a014d1ef1
Ray Wong <ray.wkwong@gmail.com>
2026-01-28 00:20:44 +0800

    basic shape and structure

--- game_env/ConnectFourEnv.py
+++ game_env/ConnectFourEnv.py
@@ -1,16 +1,74 @@
+from typing import Optional, Any
 import numpy as np
 import gymnasium as gym
+import logging
+
+DEFAULT_BOARD_ROWS = 6
+DEFAULT_BOARD_COLS = 7
 
 
 class ConnectFourEnv(gym.Env):
+    GREEN_STONE = np.array([1, 0])
+    RED_STONE = np.array([0, 1])
+    BLANK_STONE = np.array([0, 0])
+    STONE_STATE_NUM = 2
 
-    def __init__(self, size: tuple= (6,7)):
+    def __init__(self, size: tuple= (DEFAULT_BOARD_ROWS, DEFAULT_BOARD_COLS)):
         # The size of the grid (6x7 by default). i.e. 6 rows, 7 columns
         self.board_size = size
+        self.board_state = np.zeros( [self.board_size[0], self.board_size[1], 2])
 
         # Define what the agent can observe
         # Dict space gives us structured, human-readable observations
-        self.observation_space = gym.spaces.Box(0, 1, shape=(self.board_size[0],self.board_size[1]), dtype=np.float32)   # [x, y] coordinates
+        self.observation_space = gym.spaces.Box(0, 1, shape=(self.board_size[0],self.board_size[1], ConnectFourEnv.STONE_STATE_NUM), dtype=np.float32)   # [x, y] coordinates
 
         # Define what actions are available (number of columns)
         self.action_space = gym.spaces.Discrete(self.board_size[1])
+
+        self.reset()
+
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        self.board_state = np.zeros( [self.board_size[0], self.board_size[1], 2])
+        self.board_state[:,:] = ConnectFourEnv.BLANK_STONE
+
+        self.game_n_step = 0
+        info = None
+
+        # logging.info('reset hahahah')
+
+        return self.board_state , info
+
+
+    def _get_obs(self):
+        """Convert internal state to observation format.
+
+        Returns:
+            dict: Observation with agent and target positions
+        """
+        # logging.info('get obs hahahah')
+        return self.board_state
+
+    def step(self, action):
+        """Execute one timestep within the environment.
+
+        Args:
+            action: The action to take
+
+        Returns:
+            tuple: (observation, reward, terminated, truncated, info)
+        """
+        self.game_n_step = self.game_n_step + 1
+
+        reward = -0.01
+        terminated = False
+        truncated = False
+        info = { 'step' : self.game_n_step}
+        observation = self.board_state
+
+        if self.game_n_step > 10 :
+            terminated = True
+            reward = 1
+
+        # logging.info(f'get step hahahah. obs : {type(observation)},  obs shape : {observation.shape}')
+
+        return observation, reward, terminated, truncated, info
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import os
 import sys
 import logging
+from game_env.ConnectFourEnv import ConnectFourEnv
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+import torch
+
+
+import gymnasium as gym
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_vec_env
+
+from stable_baselines3.common.utils import get_device
+
+
+def init_system():
+    logging.info(f'torch version {torch.version.cuda}')
+    logging.info(f'torch cuda available: {torch.cuda.is_available()}')
+    #
+    # This will automatically use 'cuda' if a GPU is available
+    # device = get_device("auto")
+    device = get_device("cpu")
+    logging.info(f"Current device: {device}")
 
 
 def setupLogging():
@@ -24,25 +45,7 @@ def setupLogging():
     rootLogger.setLevel(logging.INFO)
 
 
-def system_init():
-    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
-def reforcement_main():
-    import torch
-
-    logging.info(f'torch version {torch.version.cuda}')
-    logging.info(f'torch cuda available: {torch.cuda.is_available()}')
-
-    import gymnasium as gym
-    from stable_baselines3 import A2C
-    from stable_baselines3.common.env_util import make_vec_env
-
-    from stable_baselines3.common.utils import get_device
-    #
-    # This will automatically use 'cuda' if a GPU is available
-    device = get_device("auto")
-    # device = get_device("cpu")
-    logging.info(f"Current device: {device}")
+def reforcement_main_test():
 
     # Parallel environments
     vec_env = make_vec_env("CartPole-v1", n_envs=4)
@@ -62,10 +65,44 @@ def reforcement_main():
         obs, rewards, dones, info = vec_env.step(action)
         vec_env.render("human")
 
+def reforcement_main():
+    # reforcement_main_test()
+
+    # environments
+    env = ConnectFourEnv()
+
+    # Set the global logging level to INFO
+    rootLogger = logging.getLogger()
+    # rootLogger.setLevel(logging.WARNING)
+
+    model = A2C("MlpPolicy", env, verbose=1)
+    # model.learn(total_timesteps=25000)
+    model.learn(total_timesteps=1000)
+    model.save("connect_four")
+
+    del model  # remove to demonstrate saving and loading
+
+
+    logging.info(f"training finished")
+    model = A2C.load("connect_four")
+
+    obs , info = env.reset()
+    terminated = False
+    truncated = False
+
+    while (not terminated) and (not truncated):
+        action, _states = model.predict(obs)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        logging.info(f'step action: {action} , info: {info}')
+        # env.render("human")
+
+    logging.info(f'end')
 
 if __name__ == "__main__":
-    system_init()
     setupLogging()
+
+    init_system()
 
     # supervised_main()
     reforcement_main()
